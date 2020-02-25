f066011cc22564ed36a8997f7eb77c795fc0f49a
Ravi Allishe <raviallishe@gmail.com>
2020-02-25 09:05:08 +0530

    Ravi #P02 - added code for penultimate problem

--- src/main/scala/easy/P01.scala
+++ src/main/scala/easy/P01.scala
@@ -7,7 +7,9 @@ object P01 extends App {
 
   def last(list: List[Int]) = {
     // list.last gives O(n) as it is singly forward linked list
-    // IndexedSeq can be used for O(1)
+    // IndexedSeq can be used for O(1) if you know the size -> https://docs.scala-lang.org/tutorials/FAQ/collections.html
+    // also if it's a doubley linked list then it's O(1) for front and back elements
+    //In scala there is only mutable doubly linked list : https://stackoverflow.com/questions/8042356/why-no-immutable-double-linked-list-in-scala-collections
     list.last
   }
 
--- src/main/scala/easy/P02.scala
+++ src/main/scala/easy/P02.scala
@@ -1,5 +1,34 @@
 package easy
 
-object P02 {
+import scala.annotation.tailrec
 
+/**
+  * Find the last but one element of a list.
+  * scala> penultimate(List(1, 1, 2, 3, 5, 8))
+  * res0: Int = 5
+**/
+
+object P02 extends App {
+
+  def penultimate(list: List[Int]) = {
+    val result = getSecondLast(list)
+    if(result.isDefined) result.get else throw new Exception("list doesn't have enough elementss")
+  }
+
+  @tailrec
+  private def getSecondLast(list: List[Int], secondLast: Option[Int] = None): Option[Int] = {
+    list match {
+      case secondLast :: _ :: Nil => Some(secondLast)
+      case _ :: tail => getSecondLast(tail, secondLast)
+      case _ => throw new Exception("list doesn't have enough elements")
+    }
+  }
+
+  println(penultimate(List(1, 1, 2, 3, 5, 8)))
 }
+
+//Notes:
+//above is the recursive approach
+//other ways using the available methods are
+//1. list.init.last
+//2. list.takeRight(n).head
